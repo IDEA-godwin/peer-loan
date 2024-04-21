70351de28ccb3d74b0b33a1e2c160de48da826ff
Godwin Igwurube <godwin.igwurube@venturec.com>
2024-04-21 01:48:19 +0100

    Strip composer default

--- packages/react-app/components/Header.tsx
+++ packages/react-app/components/Header.tsx
@@ -24,9 +24,19 @@ export default function Header() {
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 justify-between">
                             <div className="flex flex-1 items-center justify-start">
-                                <div className="flex flex-shrink-0 items-center px-2">
-                                    <h1 className="logo">EGO</h1>
+                                <div className="flex flex-shrink-0 items-center">
+                                    <h1 className="logo px-1">EGO</h1>
                                 </div>
+                            </div>
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                {!hideConnectBtn && (
+                                    <ConnectButton
+                                        showBalance={{
+                                            smallScreen: true,
+                                            largeScreen: false,
+                                        }}
+                                    />
+                                )}
                             </div>
                         </div>
                     </div>
