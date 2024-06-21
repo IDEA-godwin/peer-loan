580d74abccd5cf23239522ee6ee2a5657765b262
Godwin Igwurube <godwin.igwurube@venturec.com>
2024-06-21 01:07:35 +0100

    fix loading loan on wallet not connected

--- src/router.tsx
+++ src/router.tsx
@@ -3,34 +3,38 @@ import ProfilePage from "./pages/profile.tsx";
 import Navbar from "./components/navbar.tsx";
 import WalletConnect from "./components/wallet-connect.tsx";
 import LoansPage from "./pages/loans.tsx";
+import {useState} from "react";
 
-const AppLayout = () => (
-  <main className="d-flex flex-column flex-lg-row h-lg-100 gap-1">
-    <Navbar/>
-    <div className="flex-lg-fill overflow-x-auto ps-lg-1 vstack vh-lg-100 position-relative">
-      <WalletConnect />
-      <div
-        className="flex-fill overflow-y-lg-auto scrollbar bg-body rounded-top-4
-        rounded-top-start-lg-4 rounded-top-end-lg-0 border-top border-lg shadow-2"
-      >
-        <Outlet />
+const AppLayout = () => {
+  const [walletConnected, setWalletConnected] = useState(false);
+  return (
+    <main className="d-flex flex-column flex-lg-row h-lg-100 gap-1">
+      <Navbar/>
+      <div className="flex-lg-fill overflow-x-auto ps-lg-1 vstack vh-lg-100 position-relative">
+        <WalletConnect walletConnected={walletConnected} setWalletConnected={setWalletConnected} />
+        <div
+          className="flex-fill overflow-y-lg-auto scrollbar bg-body rounded-top-4
+          rounded-top-start-lg-4 rounded-top-end-lg-0 border-top border-lg shadow-2"
+        >
+          <Outlet context={[walletConnected]} />
+        </div>
       </div>
-    </div>
-  </main>
-)
+    </main>
+  )
+}
 
 const appRoutes = [
   {
     path: "/",
-    element: <Navigate to="/loans" replace />
+    element: <Navigate to="/loans" replace/>
   },
   {
     path: "/profile",
-    element: <ProfilePage />
+    element: <ProfilePage/>
   },
   {
     path: "/loans",
-    element: <LoansPage />
+    element: <LoansPage/>
   }
 ]
 
